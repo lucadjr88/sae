514989559a2999d73211ae3842a5497f6e873770
luca <luca@kali-MSI.luca>
2026-08-25 17:08:00 +0200

    resolver wallet pubkey

--- frontend/src/ui/elements/manualLogin.ts
+++ frontend/src/ui/elements/manualLogin.ts
@@ -6,7 +6,7 @@ export function createManualLoginElement(): HTMLDivElement {
     wrapper.id = 'ricerca_manuale';
     wrapper.innerHTML = `
       <div class="form-box centered">
-        <input type="text" id="profileId" placeholder="Player Profile ID" list="profileId-suggestions">
+        <input type="text" id="profileId" placeholder="Player Profile ID / Wallet Public Key" list="profileId-suggestions">
         <datalist id="profileId-suggestions"></datalist>
         <button id="analyzeBtn">Analyze</button>
       </div>
@@ -17,7 +17,7 @@ export function createManualLoginElement(): HTMLDivElement {
             <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"/>
           </svg>
           <p class="hompage-presentation">
-            Star Atlas Explorer is an independent, unofficial, community-driven dashboard and analytics tool. Insert your Player Profile ID to analyze your in-game operations costs and claims.
+            Star Atlas Explorer is an independent, unofficial, community-driven dashboard and analytics tool. Insert your Player Profile ID or Wallet Public Key to analyze your in-game operations costs and claims.
           </p>
         <p class="id-instructions">Check the instructions page for detailed steps.</p>
         <a href="/pages/instructions.html" target="_blank">Instructions Page</a>
